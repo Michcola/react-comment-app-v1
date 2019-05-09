8a603290d2d3df7deb96fbe464dc09e9e755a64b
mich <843298603@qq.com>
2019-05-10 01:20:36 +0800

    这是一个评论app

---
+++ build/precache-manifest.d14a25c7bf576b0bb659faeee98c7065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be47c36f0fd7ecee51a0bb4c1b053539",
+    "url": "/react-comment-app-v1/build/index.html"
+  },
+  {
+    "revision": "5935439e499b8a16d8a0",
+    "url": "/react-comment-app-v1/build/static/css/main.51befc36.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/react-comment-app-v1/build/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "5935439e499b8a16d8a0",
+    "url": "/react-comment-app-v1/build/static/js/main.0e08e66e.chunk.js"
+  },
+  {
+    "revision": "beb2f39c19955abd4613",
+    "url": "/react-comment-app-v1/build/static/js/runtime~main.67e55dd7.js"
+  }
+]);
